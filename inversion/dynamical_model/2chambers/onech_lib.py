Corrected tslip calculation in onech_lib. Found same error in disconnected_lib. Realized that  tilerr**4 in the constant factor instead of tilerr**2 in the constant factor of the likelihood function. Similarly locErr**1 instead of locErr**2 in the locations priors. Should correct for this power in disconnected_lib and connecte_lib

inversion/dynamical_model/2chambers/onech_lib.py
@@ -43,9 +43,8 @@ def DirectModelEmcee_inv_onech(tOrigTilt,tOrigGPS,
     ps = np.ones(len(tOrigTilt))
     gps = np.ones(len(tOrigGPS))
     ps0 = + 4 * alphaSamp / (1 + R1Samp)
-    PSLIP = - 2 * R1Samp * (1 - R5Samp)/(1 + R1Samp)
+    PSLIP = -4 * alphaSamp * R1Samp * (1 - R5Samp)/(1 + R1Samp)
     TSLIPcum = 0
-
     N_cycles =  ((1 + R1Samp)/ (4 * alphaSamp * R1Samp) * R3Samp)-1
     i  = 1
     w0 = [ps0]
@@ -54,7 +53,6 @@ def DirectModelEmcee_inv_onech(tOrigTilt,tOrigGPS,
         tslip,ps,gps = TwoChambers_disc_timein(w0,params,PSLIP,TSLIPcum,tOrigTilt,ps,tOrigGPS,gps,i,alphaSamp)
         ps0 =     + 4 * alphaSamp / (1 + R1Samp) -4 * alphaSamp * R1Samp * (1 - R5Samp)/(1 + R1Samp) * i
         PSLIP =   - 4 * alphaSamp * R1Samp * (1 - R5Samp)/(1 + R1Samp) * (i + 1)
-        TSLIPcum = TSLIPcum + tslip
         TSLIPcum = TSLIPcum + tslip
         w0 = np.array([ps0])
         i = i + 1
@@ -102,10 +100,10 @@ def log_likelihood_onech(param,
                                               rhog,const,S,nstation)
     sigma2Tilt = tiltErr ** 2
     sigma2GPS = GPSErr ** 2
-    liketx = -0.5 * np.sum((txObs - txMod) ** 2 / sigma2Tilt,0)  -len(txObs)/ 2 * np.log(6.28 * sigma2Tilt**2)  
-    likety = -0.5 * np.sum((tyObs - tyMod) ** 2 / sigma2Tilt,0)  -len(tyObs)/ 2 * np.log(6.28 * sigma2Tilt**2)
+    liketx = -0.5 * np.sum((txObs - txMod) ** 2 / sigma2Tilt,0)  -len(txObs)/ 2 * np.log(6.28 * sigma2Tilt)  
+    likety = -0.5 * np.sum((tyObs - tyMod) ** 2 / sigma2Tilt,0)  -len(tyObs)/ 2 * np.log(6.28 * sigma2Tilt)
     liketilt =  + liketx + likety 
-    likeGPS = -0.5 * np.sum((GPSObs - GPSMod) ** 2 / sigma2GPS) -len(GPSObs)/ 2 * np.log(6.28 * sigma2GPS**2)
+    likeGPS = -0.5 * np.sum((GPSObs - GPSMod) ** 2 / sigma2GPS) -len(GPSObs)/ 2 * np.log(6.28 * sigma2GPS)
      
     return liketilt + likeGPS
 
@@ -135,9 +133,9 @@ def log_prior_onech(param,S,rhog,bounds,boundsLoc,bndGPSconst,bndtiltconst,locTr
         conditions.append(all(np.abs(offs)<bndtiltconst))
         conditions.append(-bndGPSconst < offGPSSamp < bndGPSconst)
         if all(conditions):
-            logprob =   np.log(1.0/(np.sqrt(6.28)*locEr[0]))-0.5*(xsSamp-locTr[0])**2/locEr[0]**2
-            logprob = logprob +  np.log(1.0/(np.sqrt(6.28)*locEr[1]))-0.5*(ysSamp-locTr[1])**2/locEr[1]**2
-            logprob = logprob +  np.log(1.0/(np.sqrt(6.28)*locEr[2]))-0.5*(dsSamp-locTr[2])**2/locEr[2]**2
+            logprob =   -0.5 * np.log(6.28 *locEr[0]**2) -0.5*(xsSamp-locTr[0])**2/locEr[0]**2
+            logprob = logprob -0.5  * np.log(6.28 *locEr[1]**2) -0.5*(ysSamp-locTr[1])**2/locEr[1]**2
+            logprob = logprob -0.5  * np.log(6.28*locEr[2]**2) -0.5*(dsSamp-locTr[2])**2/locEr[2]**2
 
             return logprob
         return -np.inf

inversion/dynamical_model/2chambers/onech_plot.py
@@ -21,7 +21,7 @@ path_results = '../../../../results/onech/'
 pathrun = 'alpha'
 model_type = 'LF'
 
-stations  = ['UWD','SDH','IKI']
+stations  = ['UWD']
 date = '07-03-2018'
 flaglocation = 'N'      # This is the flag for locations priors, F for Uniform, N for Normal
 
@@ -89,7 +89,7 @@ elif Nst == 3:
     offxSamp = np.array([offx1,offx2,offx3])
     offySamp = np.array([offy1,offy2,offy3])    
 
-    txModbest,tyModbest,GPSModbest = DirectModelEmcee_inv_onech(tTilt,tGPS,
+txModbest,tyModbest,GPSModbest = DirectModelEmcee_inv_onech(tTilt,tGPS,
                                               offGPSSamp,offxSamp,offySamp,xsSamp,ysSamp,dsSamp,
                                               VsExpSamp,ksExpSamp,R5ExpSamp,R3Samp,condsSamp,alphaSamp,
                                               x,y,